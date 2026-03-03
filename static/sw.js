fbda11c31f4b7ae292a8b079bfc33f4e15379a24
fellowz_jnr <fellowz9771@gmail.com>
2026-03-03 06:33:50 +0300

    Fix PWA install prompt

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,15 @@
-self.addEventListener("install", event => {
-  console.log("TuneStreamX Service Worker installed");
+const CACHE_NAME = 'tunestreamx-v1';
+
+self.addEventListener('install', event => {
+  console.log('TuneStreamX Service Worker installed');
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('activate', event => {
+  console.log('TuneStreamX Service Worker activated');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request));
 });
